68058ea6632b808b2dde872e59159f54fa2dd7ea
intoair <1214182200@qq.com>
2015-12-27 22:11:45 +0800

    The first commit for the test

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+	<title>网址大全</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerLeft">
+            <a href="index.php"><img class="logo" src="images/logo.png" alt="" height="30" widh="150"></a>
+        </div>
+        <div class="headerRight">
+            <ul class="nav">
+                <li><a href="#cha1">重邮日常</a></li>
+                <li><a href="#cha2">屌丝培养皿</a></li>
+                <li><a href="#cha3">学霸聚集地</a></li>
+                <li><a href="#cha4">当作没有</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="chapter">
+    <!-- 第一块网址列表 -->
+    <div id="chapter1" class="banner">
+        <div class="chapterstart">
+            <p class="chname"><a name="cha1">重邮日常</a></p>
+        </div>
+        <div>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">百度</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">淘宝</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">京东</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">美团</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">支付宝</a>
+            <a href="http://baidu.com/" target="_blank" class="abc">添加网址</a>
+        </div>
+    </div>
+    <!-- 第二块网址列表 -->
+    <div>
+    <div class="chapterstart">
+        <p class="chname"><a neme="cha2">屌丝培养皿</a></p>
+    </div>
+    <div id="chapter2" class="banner">
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">LOL</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">CF</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">DNF</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">DOTA</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">GTA</a>
+            <a href="http://baidu.com/" target="_blank" class="abc">添加网址</a>        
+    </div>
+    </div>
+    <!-- 第三块网址列表 -->
+    <div id="chapter3" class="banner">
+        <div class="chapterstart">
+            <p class="chname"><a name="cha3">学霸聚集地</a></p>
+        </div>
+        <div>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">ACM</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">数学建模</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">挑战杯</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">电子设计</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">NECSS</a>
+            <a href="http://baidu.com/" target="_blank" class="abc">添加网址</a>                 
+        </div>
+    </div>
+    <!-- 第四块网址列表 -->
+    <div id="chapter4" class="banner">
+        <div class="chapterstart">
+            <p class="chname"><a name="cha4">当没有</a></p>
+        </div>
+        <div>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">组织部</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">宣传部</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">教务处</a>
+            <a href="http://202.202.43.125/" target="_blank" class="abc">人事处</a>
+            <a href="http://jwzx.cqupt.edu.cn/" target="_blank" class="abc">保卫处</a>
+            <a href="http://baidu.com/" target="_blank" class="abc">添加网址</a>               
+        </div>
+    </div>
+    </div>
+    <!-- 最后的话 -->
+    <div class="footer">
+        <div class="container">
+            <p>Powered By <a target="_blank" href="http://my.oschina.net/Sivir/blog">intoair</a>.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html> 
